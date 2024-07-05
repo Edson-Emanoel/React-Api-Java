26d5b7fb538db009243405d80967efe92ef35fee
Edson-Emanoel <156195854+Edson-Emanoel@users.noreply.github.com>
2024-07-05 11:31:43 -0300

    Update App.jsx

--- react-json/src/App.jsx
+++ react-json/src/App.jsx
@@ -6,44 +6,82 @@ function App() {
 
   const url = "http://localhost:8080/api/alunos"
 
-  const [id, setId] = useState("");
-  const [nome, setNome] = useState("")
-  const [serie, setSerie] = useState("")
-  const [sexo, setSexo] = useState("")
+  const [idState, setIdState] = useState()
+
+  const [nomeState, setNomeState] = useState("")
+  const [serieState, setSerieState] = useState("")
+  const [sexoState, setSexoState] = useState("")
 
   const [classInserir, setClassInserir] = useState("")
   const [classAlterar, setClassAlterar] = useState("sumir")
   const [data, setData] = useState([])
 
+
   useEffect(() => {
     axios.get(url  + "/todos")
-        .then(res => setData(res.data))
+    .then(res => setData(res.data))
   }, [data, setData])
 
   const Inserir = () => {
     axios.post(url + "/cadastrar", {
-      nome,
-      serie,
-      sexo
+      nomeState,
+      serieState,
+      sexoState
     })
   }
 
   const Cadastrar = (e) => {
     e.preventDefault()
 
-    if(nome === ""){
-      alert("Preencha o campo de Nome do Aluno")
-    } else if (serie === ""){
-      alert("Preencha o campo de Serie do Aluno")
-    } else if (sexo === ""){
-      alert("Preencha o campo de Sexo do Aluno")
+    if(nomeState === ""){
+      alert("Preencha o campo de NomeState do Aluno")
+    } else if (serieState === ""){
+      alert("Preencha o campo de SerieState do Aluno")
+    } else if (sexoState === ""){
+      alert("Preencha o campo de SexoState do Aluno")
     } else {
       alert("Aluno cadastrado com Sucesso!")
       Inserir()
-      setNome('')
-      setSerie('')
-      setSexo('')
+      setNomeState('')
+      setSerieState('')
+      setSexoState('')
     }
+  }
+
+  const Carregar = (id, nome, serie, sexo) => {
+
+    setClassInserir("sumir")
+    setClassAlterar("")
+
+    setIdState(id)
+    setNomeState(nome)
+    setSerieState(serie)
+    setSexoState(sexo)
+  }
+
+  const Editar = (e) => {  
+    e.preventDefault()
+
+    axios.put( `${url}/alterar/${idState}`, {
+      nomeState,
+      
+    })
+    .then( () => {
+      alert("Alterado com sucesso " + nome)
+      setNomeState(''), setIdState('');
+
+      setClassBtnInserir('')
+          setClassBtnAlterar('sumir')
+      }
+    )
+  .catch( (error) => {
+      console.log('erro: ' + error)
+  })
+
+  }
+
+  const Remover = (id) => {
+    axios.delete(`${url}/remover/${id}`)
   }
 
   return (
@@ -51,39 +89,39 @@ function App() {
       
       <h1 className="mt-5 mb-5 text-center">Cadastro de Produtos</h1>
 
-      <form className="mb-5" onSubmit={Cadastrar}>
+      <form className="mb-5">
         <div className="row mb-3">
           <div className="col">
             <input 
               type="text"
-              value={nome}
+              value={nomeState}
               placeholder="Nome do Aluno"
-              className="form-control"
-              onChange={(e) => setNome(e.target.value)}
+              className="form-control nome"
+              onChange={(e) => setNomeState(e.target.value)}
             />
           </div>
           <div className="col">
             <input 
               type="text"
-              value={serie}
+              value={serieState}
               placeholder="Serie do Aluno"
-              className="form-control"
-              onChange={(e) => setSerie(e.target.value)}
+              className="form-control serie"
+              onChange={(e) => setSerieState(e.target.value)}
             />
           </div>
           <div className="col">
             <input 
               type="text"
-              value={sexo}
+              value={sexoState}
               placeholder="Sexo do Aluno. Obs: use M e F"
-              className="form-control"
-              onChange={(e) => setSexo(e.target.value)}
+              className="form-control sexo"
+              onChange={(e) => setSexoState(e.target.value)}
             />
           </div>
         </div>
 
-        <button className={`btn btn-success ${classInserir}`}>Inserir</button>
-        <button className={`btn btn-primary ${classAlterar}`}>Salvar</button>
+        <button className={`btn btn-success ${classInserir}`} onClick={() => Cadastrar}>Inserir</button>
+        <button className={`btn btn-primary ${classAlterar}`} onClick={() => Editar}>Salvar</button>
       </form>
 
       <table className="table table-dark">
@@ -105,10 +143,10 @@ function App() {
               <td>{item.sexo}</td>
               <td>
                 <div className="btn-group d-flex gap-3">
-                  <button className="btn btn-warning">
+                  <button className="btn btn-warning" onClick={() => Carregar( item.id, item.nome, item.serie, item.sexo)}>
                     <i className="fa-solid fa-pen-to-square"></i>
                   </button>
-                  <button className="btn btn-danger">
+                  <button className="btn btn-danger" onClick={() => Remover(item.id)}>
                     <i className="fa-solid fa-trash"></i>
                   </button>
                 </div>
